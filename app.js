17fb94aa5009f095085a56c9772578c0467e3210
Subsonic-2000 <71876891+Subsonic-2000@users.noreply.github.com>
2021-09-17 22:06:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -54,8 +54,10 @@ const app = express();
 
 const PORT = process.env.PORT || 9005;
 
+//mongodb://127.0.0.1:27017
+
 //db 
-mongoose.connect('mongodb://127.0.0.1:27017',
+mongoose.connect(process.env.DATABASE_URL,
     {useNewUrlParser: true,useUnifiedTopology: true})
     .then(()=> console.log('connected to db!'))
     .then(app.listen(PORT, () =>{
